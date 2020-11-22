feb7d42d510af3910a05d62e08a893fb4a9245b2
Zeyad <ziadmamdouh24@yahoo.com>
2020-11-22 18:18:47 +0100

    Modified initializeCustomer method

--- src/com/aboushanab/Main.java
+++ src/com/aboushanab/Main.java
@@ -9,11 +9,10 @@ public class Main {
     static List<Customer> customers = new ArrayList<>();
     static List<Reservation> reservations = new ArrayList<>();
     static List<String> tasks = new ArrayList<>();
-
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         addCars();
         addTasks();
-        Scanner input = new Scanner(System.in);
         while (true) {
             System.out.println("\t\t\t\t\tWelcome to Car Rental App!\n Please choose one of the following! \n" +
                     "1- Customer \t\t 2- Admin");
@@ -28,65 +27,19 @@ public class Main {
         }
     }
     public static void initializeCustomer() {
-        int i;
-        boolean validDate = false;
-        boolean validCar = false;
-        Car car = new Car();
-        String date;
-        Date date2 = null;
         Scanner input = new Scanner(System.in);
-        while (!validDate) {
-            System.out.println("Please enter a date in the form of dd-mm-yyyy");
-            date = input.next();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-            try {
-                //Parsing the String
-                date2 = dateFormat.parse(date);
-                validDate = true;
-            } catch (ParseException e) {
-                System.out.println("Please enter a valid date!");
-            }
-        }
+        Date date = validateDate();
         System.out.println("Here's a list of the available cars!");
-        while (!validCar) {
-            for (i = 0; i < cars.size(); i++) {
-                System.out.println(i + 1 + "-" + cars.get(i).toString() + "\n");
-            }
-            System.out.println("Please enter the id of the car you want (Only numbers allowed): ");
-            String carID = input.next();
-            int carIDInt = Integer.parseInt(String.valueOf(carID));
-            if (carIDInt > 0 && carIDInt <= cars.size()) {
-                for (i = 0; i < cars.size(); i++) {
-                    if (carIDInt == cars.get(i).id) {
-                        car = cars.get(i);
-                        validCar = true;
-                    }
-                }
-            } else {
-                System.out.println("Please enter a valid ID!");
-            }
-        }
-
+        Car car = validateCar();
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date2);
-        Customer customer = new Customer();
-        System.out.println("Please enter your details\n Name: ");
-        customer.name = input.next();
-        System.out.println("E-mail: ");
-        customer.email = input.next();
-        System.out.println("Address: ");
-        customer.address = input.next();
-        System.out.println("Title: ");
-        customer.title = input.next();
-        System.out.println("Phone: ");
-        customer.phone = input.next();
-        customer.id = customers.size()+1;
+        cal.setTime(date);
+        Customer customer = createCustomer();
         customers.add(customer);
         System.out.println("How many days would you like to reserve the car? (In numbers!): ");
         int days = Integer.parseInt(String.valueOf(input.next()));
         cal.add(Calendar.DAY_OF_MONTH, days);
-        Date date3 = cal.getTime();
-        Reservation reservation = new Reservation(customer, reservations.size()+1, car, date2, date3);
+        Date date2 = cal.getTime();
+        Reservation reservation = new Reservation(customer, reservations.size()+1, car, date, date2);
         System.out.println("Total price will be: " + days * car.price + "$\tType 'ok' to confirm or anything to cancel");
         if (input.next().equalsIgnoreCase("ok")) {
             reservations.add(reservation);
@@ -174,6 +127,7 @@ public class Main {
                 System.out.println("Invalid choice");
         }
     }
+
     public static void addCars() {
         cars.add(new Car("Toyota\t", cars.size() + 1, "\\ō͡≡o˞̶", 23, true));
         cars.add(new Car("Huyndai\t", cars.size() + 1, "\\ō͡≡o˞̶", 30, true));
@@ -205,7 +159,9 @@ public class Main {
     public static void printCars(){
         int i;
         for(i=0;i<cars.size();i++){
-            System.out.println((i+1)+"-"+cars.get(i).toString()+"\n");
+            if(cars.get(i).availability==true) {
+                System.out.println((i + 1) + "-" + cars.get(i).toString() + "\n");
+            }
         }
 
     }
@@ -226,5 +182,61 @@ public class Main {
             }
         }
         return null;
+    }
+    public static Date validateDate(){
+        String date;
+        boolean validDate = false;
+        Date date2 = null;
+        while (!validDate) {
+            System.out.println("Please enter a date in the form of dd-mm-yyyy");
+            date = input.next();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            try {
+                //Parsing the String
+                date2 = dateFormat.parse(date);
+                validDate = true;
+                return date2;
+            } catch (ParseException e) {
+                System.out.println("Please enter a valid date!");
+            }
+        }
+        return null;
+    }
+    public static Car validateCar(){
+        int i;
+        boolean validCar = false;
+
+        while (!validCar) {
+            printCars();
+            System.out.println("Please enter the id of the car you want (Only numbers allowed): ");
+            String carID = input.next();
+            int carIDInt = Integer.parseInt(String.valueOf(carID));
+            if (carIDInt > 0 && carIDInt <= cars.size()) {
+                for (i = 0; i < cars.size(); i++) {
+                    if (carIDInt == cars.get(i).id) {
+                        validCar = true;
+                        return cars.get(i);
+                    }
+                }
+            } else {
+                System.out.println("Please enter a valid ID!");
+            }
+        }
+        return null;
+    }
+    public static Customer createCustomer(){
+        Customer customer = new Customer();
+        System.out.println("Please enter your details\n Name: ");
+        customer.name = input.next();
+        System.out.println("E-mail: ");
+        customer.email = input.next();
+        System.out.println("Address: ");
+        customer.address = input.next();
+        System.out.println("Title: ");
+        customer.title = input.next();
+        System.out.println("Phone: ");
+        customer.phone = input.next();
+        customer.id = customers.size()+1;
+        return customer;
     }
 }
